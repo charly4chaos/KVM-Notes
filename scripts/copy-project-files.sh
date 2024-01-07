d1cba82218d98abfe60891861450afcb78456745
Charly4Chaos <charly4chaos@b2tr.de>
2024-01-07 16:07:09 +0900

    Correct permissions

--- scripts/copy-project-files.sh
+++ scripts/copy-project-files.sh
@@ -10,14 +10,18 @@ function exit_with_message {
 case ${COMMAND} in
     "hooks")
         rsync -rv ${PROJECTDIR}/root/etc/libvirt/hooks/ /etc/libvirt/hooks || exit_with_message "Error copying files" 1
-        find /etc/libvirt/hooks -type f -exec chmod 744 {} \; 
+        find /etc/libvirt/hooks -type f -exec chmod 755 {} \; 
         find /etc/libvirt/hooks -type d -exec chmod 755 {} \; 
         find /etc/libvirt/hooks         -exec chown root:root {} \; 
+        echo Files:
+        ls -l /etc/libvirt/hooks/*
         ;;
     "udev")
         rsync -rv ${PROJECTDIR}/root/etc/udev/rules.d/ /etc/udev/rules.d || exit_with_message "Error copying files" 1
         find /etc/libvirt/hooks -type f -exec chmod 644 {} \; 
         find /etc/libvirt/hooks         -exec chown root:root {} \; 
+        echo Files:
+        ls -l /etc/udev/rules.d/*
         ;;
     *) 
     echo "Command ${COMMAND} unknown."
